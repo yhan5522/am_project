2d361710c72bfe56acfbf27d9fc39f9040db3688
yhan5522 <gks963@naver.com>
2019-10-10 00:12:07 +0900

    조회 메소드

--- am_project/src/book/BookDAO.java
+++ am_project/src/book/BookDAO.java
@@ -3,7 +3,10 @@ package book;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookDAO {
 
@@ -42,5 +45,54 @@ public class BookDAO {
 				e.printStackTrace();
 			}
 		}
+	}
+	
+	// 목록 조회
+	public List<BookDTO> getDBList(String searchBookName) {
+		
+		connect();
+		
+		List<BookDTO> bookList = new ArrayList<BookDTO>();
+		
+		String sql = "select bookNumber, bookName, author, publishingHouse, category, symbol, status from book where bookName like ?";
+		
+		try {
+			
+			pstmt = conn.prepareStatement(sql);
+			
+
+			// SQL문에 변수 입력
+			
+			pstmt.setString(1,searchBookName+"%");
+
+			//SQL문 실행
+			ResultSet rs = pstmt.executeQuery();
+
+			while(rs.next()) {
+				
+				// DTO 객체 생성
+				BookDTO bookDTO = new BookDTO();
+				
+				// DB Select결과를 DO 객체에 저장
+				bookDTO.setBookNumber(rs.getInt("booknumber"));
+				bookDTO.setBookName(rs.getString("bookName"));
+				bookDTO.setAuthor(rs.getString("author"));
+				bookDTO.setPublishingHouse(rs.getString("publishingHouse"));
+				bookDTO.setCategory(rs.getString("category"));
+				bookDTO.setSymbol(rs.getString("symbol"));
+				bookDTO.setStatus(rs.getString("status"));
+				
+							
+				bookList.add(bookDTO);
+			}
+			rs.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			disconnect();
+		}
+		return bookList;
 	}
 }
