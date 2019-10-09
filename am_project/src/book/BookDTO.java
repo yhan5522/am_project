36021afeeab49f74d396f5eccd3844143e4670b8
yhan5522 <gks963@naver.com>
2019-10-09 23:40:56 +0900

    DTO 작성

--- am_project/src/book/BookDTO.java
+++ am_project/src/book/BookDTO.java
@@ -2,4 +2,62 @@ package book;
 
 public class BookDTO {
 
+	private int bookNumber;
+	private String bookName;
+	private String author;
+	private String publishingHouse;
+	private String category;
+	private String symbol;
+	private String status;
+	private String searchBookName;
+	
+	public String getSearchBookName() {
+		return searchBookName;
+	}
+	public void setSearchBookName(String searchBookName) {
+		this.searchBookName = searchBookName;
+	}
+	public int getBookNumber() {
+		return bookNumber;
+	}
+	public void setBookNumber(int bookNumber) {
+		this.bookNumber = bookNumber;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPublishingHouse() {
+		return publishingHouse;
+	}
+	public void setPublishingHouse(String publishingHouse) {
+		this.publishingHouse = publishingHouse;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
 }
