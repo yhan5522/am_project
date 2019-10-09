14f6bd61c9e379066112616434d99829fa20ff47
yhan5522 <gks963@naver.com>
2019-10-10 00:09:41 +0900

    DB 연결 및 해제 메소드

---
+++ am_project/src/book/BookDAO.java
@@ -0,0 +1,46 @@
+package book;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class BookDAO {
+
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+
+	// MySQL 연결정보 
+	String jdbc_driver = "com.mysql.jdbc.Driver";
+	
+	String jdbc_url = "jdbc:mysql://127.0.0.1/jspdb?useSSL=true&verifyServerCertificate=false&serverTimezone=UTC";
+
+	// DB 연결
+	void connect() {
+		try {
+			Class.forName(jdbc_driver);
+
+			conn = DriverManager.getConnection(jdbc_url,"jspbook","1234");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// DB 연결해제
+	void disconnect() {
+		if(pstmt != null) {
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} 
+		if(conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
